c681741f3245ad4ad633c31a82eb7328ec4d46d8
Rod Gaitan <rodgaitan01@gmail.com>
2026-03-22 00:18:42 -0400

    working on defining the schema and compiling it into a model

--- models/Product.js
+++ models/Product.js
@@ -14,3 +14,17 @@ const User = mongoose.model('User', userSchema); //You need to compile it into a
                                                   // - 'User' — the name of the model. Mongoose will automatically look for a collection called 'users' in MongoDB (lowercased + pluralized)
                                                   //- 'userSchema' — the shape/structure that documents in that collection should follow
 */
+
+// Defining a schema called 'productSchema' 
+const productSchema = new mongoose.Schema({  //  defines exactly what fields a document can have, what types they must be, and what rules they must follow.
+    name:     	{ type: String, required: true },
+    description:{ type: String, required: true },
+    price:		{type: Number, required: true, $gt: 0},
+    category: 	{ type: String, required: true},
+    inStock: 	{ type: Boolean, defaults: true},
+    tags: 		{},
+    createdAt: 	{ type: Date, default: Date.now }
+})
+
+// Compiling the schema 'productSchema' into a model named 'Product' and exporting it.
+const Product = mongoose.model('Product', productSchema);
