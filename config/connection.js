743b64654112be680f2c9358df68cdba224fd4d5
Rod Gaitan <rodgaitan01@gmail.com>
2026-03-22 21:19:57 -0400

    completed session connection to MongoDb - Task 3

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,40 @@
 // This is what starts a connection (SESSION) between your 'app' and 'MongoDB'.
 // You run it with 'node index.js'   
 
-// Imports 'mongoose' assuming already installed
 //require('dotenv').config();  // REMOVED!!!! as dotenv already being called in 'server.js' when project broken up into separate files.
                             // It loads '.env' file, so 'process.env.MONGODB_URI' is available
                             // it is your MongoDB connection string.
 const mongoose = require('mongoose'); //Imports 'Mongoose' in to the file for use
 
+/* Works but offers no graceful error handling. Instead use code block below
     //async function main() {
    async function connectDB() {
-  //await mongoose.connect(process.env.MONGODB_URI);
   await mongoose.connect(process.env.MONGO_URI);  // opens connection session through which the app file can starts CRUD stuff on the MongoDb.
   console.log('Connected to MongoDB via Mongoose!'); //consoles to the terminal if connection successfully.
 }
+*/
+
+const connectDB = async () => { //wrapping this in 'async' function because connecting to a database takes time. Allows me to use 'await'
+    try{  // TRY the following code.....
+        const connecting = await mongoose.connect(process.env.MONGO_URI) // This is the  actual connection call.
+                                                                         // pulls the connection string from your .env file allowing me to connect with with my pwd WITHOUT exposing it 
+                                                                        // - 'await' = pauses execution until the connection is made, before moving on.
+        console.log(`MongoDb connected!: ${connecting.connection.host}`)  // IF code runs successfully, THEN console log success message and its Atlas cluster address ( ${connecting.connection.host}).
+    } catch(e) {  // ELSE_IF anything else goes wrong like wrong password, no internet, bad URI, etc, THEN the catch block intercepts the error so the app doesn't just crash SILENTLY....
+        //console.log(`Error connecting to MongoDB: ${e.message}`)
+        console.error(`Error connecting to MongoDB: ${e.message}`) //RATHER it tells teh user what might be wrong.
+        process.exit(1) // this is for when if MongoDB fails to connect, the server shuts down immediately with a clear error message
+                        // wihtout this line, the server will still start and run, but when user hits any API endpoint, they'll get a confusing error.
+    }
+}
+
+module.exports = connectDB  // exports the file/function so that 'server.js' entry point file can import, call, and use it.
+
+
+
+
+
+///////////////////// Scrap book ///////////
 
 /*
 const PORT = process.env.PORT || 5000;
@@ -21,6 +43,3 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 */
-
-
-module.exports = connectDB  // exports the file so that 'server.js' entry point file can use it
--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 
 // My entry-point file
 
-require('dotenv').config();
-const express = require('express');
-const connectDB = require('./config/connection');
+require('dotenv').config(); //this loads the  '.env' file at start up, so every file in the app has access to 'process.env' env variables
+const express = require('express'); // loads Express to your app after having been installed
+const connectDB = require('./config/connection');   // imports a database connection function 'connectDB' from file './config/connection.js.' aand giving it the same name 'connectDB'
 
 
-const app = express();
+const app = express(); // - 'express()' = function that comes from the Express library. 
+                        // When you call it, it creates a complete 'Expressapplication object' and stores it in the variable 'app'.
+                        // allowing 'app' to become sota like UI upon which you can adapt functions (.use, .get, .post, .listen, etc) that create the methods you use with your API
+                        // without this line, 'app' would just be 'undefined'.
 
 // Middleware
-app.use(express.json());
+app.use(express.json()); // this middleware tells 'Express' to automatically parse incoming request bodies as JSON. 
+                        // without this, when someone sends a POST request (to my API) with product data, 'req.body' would be 'undefined'.
 
 // Connect to database
-connectDB();
+connectDB();  // Calls the function 'connectDB()' in 'connection.js' to establish a DB connection when the server starts.
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;  // checks if there's a PORT variable defined in the .env file. IF there is one, it'll use it. ELSE, if there isn't one, it'll use default 5000.
+app.listen(PORT, () => { // this starts the server and starts listening for incoming API requests on that port. 
+                        // 'app' is defined in line 'const app = express();'.
+console.log(`Server running on port ${PORT}`);  // The callback function THEN fires and executes the code that console logs a connection message, once the server is successfully up.
 });
