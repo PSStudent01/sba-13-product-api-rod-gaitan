ee6fae7ec2c25ca445fb25942972a831d5920d5f
Rod Gaitan <rodgaitan01@gmail.com>
2026-03-23 00:19:56 -0400

    finished DELETE route - Task 4

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,50 +3,147 @@ const express = require('express'); // loads Express to your app after having be
 const Product = require('../models/Product'); //'productSchema' having been converted from a schema into a model 'Product', now we import it, so that it can interact with DB
 const router = express.Router();  // this creates a mini router object that holds all of the needed routes
 
-// POST /api/products - to create a new product
-router.post('/', async (req, res) => { 
-    try{
-        const product = await Product.create(req.body)
-        res.status(201).json(product);
-    } catch(err){
+// Route POST /api/products - to create a new product
+router.post('/', async (req, res) => { // creates a POST route..
+    try{                                 //...while gracefully handling errors....
+        // const product = await Product.create(req.body)
+        const product = new Product(req.body);  //...where IF it 'imports' the 'Product' model successfully...
+        const savedProduct = await product.save(); //... THEN 1)it waits and 2)if the 'Product' model (savedProduct) is valid, 3)it saves data to DB...
+        res.status(201).json(savedProduct); // ...and 4)returns a 201 code (201 = 'Created')- record successfully 'created'!
+    } catch(err){ //...ELSE
           //res.status(400).json(err);
-          res.status(400).json({ message: err.message});
+          res.status(400).json({ message: err.message}); //returns a 400 error code (400 = 'Bad Request')-  record creation 'failed'! 
+                                                         // chose '(err)' & 'message: err.message' bc many thing scan go wrong here and hardcoding it wouldnt cover them all   
+
     }
 })
-
-
-
-
-    /*
-// POST /api/notes - Create a new note
-router.post('/', async (req, res) => {
-  try {
-    const note = await Note.create({
-      ...req.body,
-      // The user ID needs to be added here
-    });
-    res.status(201).json(note);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
+/*
+Testing parameters:
+Method: POST
+URL: http://localhost:5000/api/products
+Body: raw > JSON
+- tested both for both status codes:
+-- 201
+-- 400
+- MongoDB correctly updated
 */
 
 
+// Route GET /api/products/:id - to fetch a single product by ID
+router.get('/:id', async (req, res) => { // creates a GET route...
+    try{      //...while gracefully handling errors...
+        const product = await Product.findById(req.params.id); //...where IF it 'finds' the 'Product' model successfully..
+        if (!product) {                                         //...BUT the specific product is not valid/found..
+            return res.status(404).json({ message: 'Product not found' }); //...it returns a 404 error code (404 = 'Product not found') 
+        }
+        res.status(200).json(product); //...ELSE IF product found, it returns a 200 code (200 = OK) - specific record successfully 'found'!!
+    } catch{                         
+        //res.status(400).json({ message: err.message });
+        res.status(400).json({ message: 'Invalid ID format' })  //...ELSE returns a 400 error code (400 = 'Invalid ID format') 
+    }
+})
+/*
+Testing parameters:
+Method: GET
+Body: raw > JSON
+#
+Test 1 — Valid ID that exists:
+GET http://localhost:5000/api/products/69c09ec02259995c462a474c
+Returned: 200 
+#
+Test 2 —  Valid format, but ID doesn't exist:
+GET http://localhost:5000/api/products/111111111111111111111111
+Returned: 404 
+#
+Test 3 — Invalid format:
+GET http://localhost:5000/api/products/banana
+Returned: 400
+*/
+
+// Route Update / PUT (/api/books/:id) for 1 single product document
+router.put('/:id', async (req, res) => {   // creates a PUT route...
+  try {                                  //...while gracefully handling errors....
+    const updatedProduct = await Product.findByIdAndUpdate(  //...where IF it 'finds' ....
+      req.params.id,  //...the 'Product' model by id successfully...
+      req.body,      // ...THEN it replaces its data with what the client sent..
+      //{ new: true, runValidators: true}  // ...and then returns the updated version, not the old one... 
+      { returnDocument: 'after', runValidators: true }
+      //{returnDocument: 'after'}
+    );
+    if (!updatedProduct) { // however IF the 'updatedBook'...
+      return res.status(404).json({ message: 'Product not found' }); //...THEN  404 error code is generated (404 = Not Found)...
+    }
+    res.status(200).json(updatedProduct);  //...ELSE IF 'updatedProduct' is valid THEN  200  code is generated (200 = OK)...
+  } catch (error) {  //ELSE....
+    res.status(400).json({ message: error.message });  // 400  error code is generated
+  }
+});
+
+/*
+Testing parameters:
+Method: PUT
+Body: raw > JSON
+{
+    "price": 159.99,
+    "inStock": true
+}
+#
+Test 1 — Successful product update:
+http://localhost:5000/api/products/69c09ec02259995c462a474c
+Returned: 200 
+#
+Test 2 — Product not found:
+http://localhost:5000/api/products/111111111111111111111111
+Returned: 404 
+#
+Test 3 — Validation:
+{
+    "price": -10
+}
+http://localhost:5000/api/products/69c09ec02259995c462a474c
+Returned: 400 
+"Validation failed: price: Price must be greater than 0"
+*/
 
 
-
-
-
-
-
-
-
-
+// DELETE /api/products/:id - Delete a product
+router.delete('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'No product found with this id!' });
+    }
+    res.status(200).json({ message: 'Product deleted!' });
+  } catch {
+    //res.status(400).json(err);
+    res.status(400).json({ message: 'Invalid ID format'});
+  }
+});
+/*
+Testing parameters:
+Method: DELETE
+Body: raw > JSON
+#
+Test 1 — Successful product delete:
+http://localhost:5000/api/products/69c09ec02259995c462a474c
+Returned: 200 OK
+#
+Test 2 — Product not found:
+http://localhost:5000/api/products/111111111111111111111111
+Returned: 404 Not Found
+#
+Test 3 — Invalid format:
+http://localhost:5000/api/products/banana
+Returned: 400 
+*/
 
 // Ib)
 module.exports = router; // exports router so 'server.js' can use it
+
+
+
+
+
 
 /*
 // Apply authMiddleware to all routes in this file
